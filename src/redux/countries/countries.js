7611e4cd04adf7e9862c5406b37ac86b6d7bf983
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-08-31 09:49:39 +0000

    add action listeners

---
+++ src/redux/countries/countries.js
@@ -0,0 +1,28 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+
+const initialState = [];
+const GET_COUNTRIES = 'GET_COUNTRIES';
+const SEARCH_COUNTRIES = 'SEARCH_COUNTRIES';
+
+const getCountries = createAsyncThunk(GET_COUNTRIES, async () => {
+  const response = await axios.get('https://restcountries.eu/rest/v3.1/all');
+  return response.data;
+});
+
+const searchCountries = createAsyncThunk(SEARCH_COUNTRIES, async (search) => {
+  const response = await axios.get(`https://restcountries.eu/rest/v3.1/name/${search}`);
+  return response.data;
+});
+
+const countriesSlicer = createSlice({
+  name: 'countries',
+  initialState,
+  extraReducers: {
+    [getCountries.fulfilled]: (state, action) => {
+      state = action.payload
+    },
+    [searchCountries.fulfilled]: (state, action) => {
+      state = action.payload
+    },
+  },
+});
