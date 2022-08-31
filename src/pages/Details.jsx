8a8e27c2d3337a99bbd4a68e07cf34b628432762
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-08-31 15:44:45 +0000

    add pages but under construction

---
+++ src/pages/Details.jsx
@@ -0,0 +1,12 @@
+import './css/details.css';
+import React from "react";
+
+const Details = () => {
+  return (
+    <div className="details">
+      <h1>Details</h1>
+    </div>
+  );
+}
+
+export default Details;
