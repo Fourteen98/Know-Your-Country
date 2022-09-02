408a6e6a71a3482a827863aa70aa76687b261d78
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-09-02 11:47:51 +0000

    fix linter

--- src/test/details.test.js
+++ src/test/details.test.js
@@ -1,9 +1,10 @@
-jest.useFakeTimers()
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import Details from '../pages/Details';
 import store from '../redux/configureStore';
+
+jest.useFakeTimers();
 
 it('Continents render correctly', () => {
   const tree = TestRenderer.create(
